cca895b3813e7f2236bcbd68144c11507388aaf6
4419-TEACHER\user <jasontung2214@gmail.com>
2017-10-28 14:51:58 +0800

    1. 設定audioMixer 未完成

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+    private Canvas canvas;
+    private void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvas.enabled = !canvas.enabled;
+            Pause();
+        }
+	}
+
+    public void Pause()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        //if(Time.timeScale == 0)
+        //{
+        //    Time.timeScale = 1;
+        //}
+        //else
+        //{
+        //    Time.timeScale = 0;
+        //}
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
