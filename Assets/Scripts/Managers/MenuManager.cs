3b83f4fc521c1e87d1f7e287363413b8794c4d55
4419-TEACHER\user <jasontung2214@gmail.com>
2017-11-11 14:22:48 +0800

    完成audioMixer設定

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class MenuManager : MonoBehaviour {
     private Canvas canvas;
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unPaused;
     private void Awake()
     {
         canvas = GetComponent<Canvas>();
@@ -21,14 +23,19 @@ public class MenuManager : MonoBehaviour {
     public void Pause()
     {
         Time.timeScale = Time.timeScale == 0 ? 1 : 0;
-        //if(Time.timeScale == 0)
-        //{
-        //    Time.timeScale = 1;
-        //}
-        //else
-        //{
-        //    Time.timeScale = 0;
-        //}
+        LowPass();
+    }
+    
+    private void LowPass()
+    {
+        if(Time.timeScale == 0)
+        {
+            paused.TransitionTo(0.01f);
+        }
+        else
+        {
+            unPaused.TransitionTo(0.01f);
+        }
     }
 
     public void Quit()
